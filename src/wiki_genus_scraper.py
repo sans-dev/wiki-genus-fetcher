07c2b8857d0408a4257b32984561e6febeeaa424
ssander@GWS1 <ssander@uni-bonn.de>
2024-03-18 09:52:03 +0100

    add CLI

--- src/wiki_genus_scraper.py
+++ src/wiki_genus_scraper.py
@@ -2,20 +2,13 @@ import requests
 import json
 import pandas as pd
 from tqdm import tqdm
+import argparse
+from configparser import ConfigParser
 import sys
-sys.setrecursionlimit(10000) 
+sys.setrecursionlimit(10000)
 
-page = 'insecta'
-base_url = 'https://species.wikimedia.org/w/rest.php/v1/page/'
-url = base_url + page
-headers = {
-  'Authorization': 'Sebsan 90 eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxMDE1ZmU3NTNjZGMxZTJmYzkxY2Y2ZGU0OTk4OGZjZSIsImp0aSI6IjlkNGE5ZTZiZWQwOGNiNTcxZmUyODdhYTNjYTc5NWU0Yjk5ZTBiZjMxZTI4MjdjODYwYzQ0YWRjZDRmZDI3MmRmZTVmNTAyMDdkY2NkOWUzIiwiaWF0IjoxNzEwNDAzMzI5LjE0NTg4NiwibmJmIjoxNzEwNDAzMzI5LjE0NTg5LCJleHAiOjMzMjY3MzEyMTI5LjE0MzI4LCJzdWIiOiI3NTE5ODgwNSIsImlzcyI6Imh0dHBzOi8vbWV0YS53aWtpbWVkaWEub3JnIiwicmF0ZWxpbWl0Ijp7InJlcXVlc3RzX3Blcl91bml0Ijo1MDAwLCJ1bml0IjoiSE9VUiJ9LCJzY29wZXMiOlsiYmFzaWMiXX0.QEQgd2mCuXrxTbdDjRKsUtZVlY_TTwroL-J3Chh9ByzTRjI_kD7AAdx45vAnagzKQU6e9gL09DmvUS8C1TkY_YCzimgq5L4-58X7qdy7bkmASwj-DX6POgQrI12r1iNwHwnNYn4OqHVrDk0I8WfKrNRnNjX0xJ-i6NVuIGAHkJBRjXhiazw_d6J3YzQ68MplNSXgXCThj8vV_E7VlFAFXQ9eTpLCfqMSV9Lk09n7g8lk2_NcxHXqyI3tmRGeM8PdulQi7RBGCoCjoy2o8Qv1yJ5YnQtOQ9AJwA4MTexSipW6dMK6mD12k05rdI7bpjtnhXUOHXg0jLAhQ4VLBJRNpN1A96NXHj9IHiqpdBy3aQyZpMt1NtbWeT1DJHC1HPZVe-iHf_5-hzqBRwJi2cR23q_QBXgI_JaT_bMDVWsBXHQFmFxtmpHKzNoUqXSajY5yd1wMbnF2xdZenNDIJHvotpEUEYaQ-AYlyjz7fKahj92vmX6EAurv8hP5Yc5uMlj9Rf_4qxNuMOStHEyO4NA0fDD2aKVNZEN3O0pRCFiroBHNXTSPBjKlNoFfSk8QeW2-RH8HJJd5XEr0gJXJHrleSue_pCJZRcJwMw5crd0QoBNmeVQF7x8Qy3df2udqyH3u9huRmjoOIdpintYcWtL-10G6t8wUPTl7PaeeQoH71ys',
-  'User-Agent': 'Genus (seppelpie@googlemail.com)'
-}
-
-response = requests.get(url, headers=headers)
-insecta_page = json.loads(response.text)
-source_str = insecta_page['source'].splitlines()
+BASE_URL = 'https://species.wikimedia.org/w/rest.php/v1/page/'
+HEADERS = {}
 
 def get_taxon_name(substring, taxon_level):
     if taxon_level in substring:
@@ -25,10 +18,28 @@ def get_taxon_name(substring, taxon_level):
         return taxon_name
     else: return None
 
-def get_species_count(taxon_name, base_url, headers, pbar, species_count={}):
-    url = base_url + taxon_name
+def get_species_count(taxon_name, pbar, species_count={}):
+    """ This function fetches and counts the number of species under a given taxon from a web resource.
+
+    The function takes the following parameters:
+
+    taxon_name (str): The name of the taxon for which the species count is required.
+    pbar (ProgressBar object): A progress bar object to track the progress of the operation.
+    species_count (dict, optional): An existing dictionary to which the count of species for the taxon will be added. If not provided,
+    a new dictionary will be created.
+
+    The function uses the requests library to send an HTTP GET request to the web resource and parses the response using the json library. 
+    It then checks the 'source' field in the response for certain keys 
+    ('{sp', '{g', '{fam', '{superfam', '{subordo') that indicate the presence of species, genus, family, superfamily, or suborder information, respectively.
+    For each key found, the function counts the number of lines in the 'source' field that contain that key and adds the count to the species_count dictionary 
+    with the taxon_name as the key. If a key is found that matches the taxon_name, the function stops counting and returns the species_count dictionary.
+    If no keys are found in the 'source' field, the function returns an empty dictiotaxon
+    Note: This function is recursive, meaning it calls itself when it finds a key in the 'source' field that doesn't match the taxon_name. 
+    The recursion stops when a key matching the taxon_name is found or when all lines in the 'source' field have been checked. """
+    
+    url = BASE_URL + taxon_name
     pbar.set_description(f"fetching results for {taxon_name}...")
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=HEADERS)
     content = json.loads(response.text)
     if 'source' in content.keys():
         source = content['source']          
@@ -46,50 +57,79 @@ def get_species_count(taxon_name, base_url, headers, pbar, species_count={}):
                 if genus:
                     if genus == taxon_name:
                         return
-                    get_species_count(genus,base_url,headers, pbar, species_count)
+                    get_species_count(genus, pbar, species_count)
         if '{fam' in source:
             for line in source.splitlines():
                 fam = get_taxon_name(line,'{fam')
                 if fam:
                     if fam == taxon_name:
                         return
-                    get_species_count(fam, base_url, headers, pbar, species_count)
+                    get_species_count(fam, pbar, species_count)
         if '{superfam' in source:
             for line in source.splitlines():
                 superfam = get_taxon_name(line,'{superfam')
                 if superfam:
                     if superfam == taxon_name:
                         return
-                    get_species_count(superfam, base_url, headers, pbar, species_count)                    
+                    get_species_count(superfam, pbar, species_count)                    
         if '{subordo' in source:
             for line in source.splitlines():
                 suborder = get_taxon_name(line,'{subordo')
                 if suborder:
                     if suborder == taxon_name:
                         return
-                    get_species_count(suborder, base_url, headers, pbar, species_count)
+                    get_species_count(suborder, pbar, species_count)
         return species_count
     else:
         return
+
+def main(args):
+    # build header from config
+    config = ConfigParser()
+    config.read(args.config)
+    config = config[args.section]
+
+    auth_str = config['user_name'] + config['api_token']
+    user_agent_str = config['user_agend'] + f"({config['email']})"
+
+    HEADERS = {
+        'Authorization' : auth_str,
+        'User-Agent' : user_agent_str
+    }
+
+    url = BASE_URL + args.start_taxon
+
+    response = requests.get(url, headers=HEADERS)
+    init_page = json.loads(response.text)
+    source_str = init_page['source'].splitlines()
+
+    taxon_names = []
+    for substring in source_str:
+        taxon_name = get_taxon_name(substring, taxon_level='ordo')
+        if taxon_name:
+            taxon_names.append(taxon_name.replace('"',''))
     
-order_names = []
-for substring in source_str:
-    order_name = get_taxon_name(substring, taxon_level='ordo')
-    if order_name:
-        order_names.append(order_name.replace('"',''))
+    species_count = {}
+    pbar = tqdm(taxon_names)
+    for order in pbar:
+        print(f"processing {order}...")
+        species_count[order] = get_species_count(order, pbar)
 
-species_count = {}
-pbar = tqdm(order_names)
-for order in pbar:
-    print(f"processing {order}...")
-    species_count[order] = get_species_count(order, base_url, headers, pbar)
+    with open('species_count.json', 'w') as f:
+        json.dump(species_count, f, indent=4)
 
-with open('species_count.json', 'w') as f:
-    json.dump(species_count, f, indent=4)
+    # convert dictionary to list of tuples
+    data = [(order, genus, count) for order in species_count for genus, count in species_count[order].items()]
 
-# convert dictionary to list of tuples
-data = [(order, genus, count) for order in species_count for genus, count in species_count[order].items()]
+    # create dataframe
+    species_df = pd.DataFrame(data, columns=['Order', 'Genus', 'species_count'])
+    species_df.to_csv('species_count.csv')
 
-# create dataframe
-species_df = pd.DataFrame(data, columns=['Order', 'Genus', 'species_count'])
-species_df.to_csv('species_count.csv')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="This python program fetches the number of species for all genera within a start taxon.")
+    parser.add_argument("--config", type=str, help="Path of the config file containing the user credentials for wikimedia.")
+    parser.add_argument("--section", type=str, default="WIKI", help="Section of the configuration. Default is 'WIKI'.")
+    parser.add_argument("--start-taxon", type=str, help="Initial Taxon from which species numbers will be fetched for each genus.")
+    args = parser.parse_args()
+    main(args)
